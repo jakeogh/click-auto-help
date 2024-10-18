549063b93fadfcf14a22dc7e05dc56176dc32ffd
jakeogh <github.com@v6y.net>
2024-10-18 00:09:50 -0700

    auto-commit

--- click_auto_help/__init__.py
+++ click_auto_help/__init__.py
@@ -1,2 +1,6 @@
-from .click_auto_help import AHCommandCollection
-from .click_auto_help import AHGroup
+"""
+isort:skip_file
+"""
+
+from .click_auto_help import AHCommandCollection as AHCommandCollection
+from .click_auto_help import AHGroup as AHGroup
